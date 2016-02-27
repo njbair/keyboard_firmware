4b045f5435e7fd3ef9fa71e2b497f55c4ad945b6
Nick Bair <njbair@gmail.com>
2016-02-27 00:08:25 -0500

    update AEKII layout. now with magic!!!

--- keyboard/alps64/keymaps/aekii.c
+++ keyboard/alps64/keymaps/aekii.c
@@ -1,52 +1,61 @@
 #include "keymap_common.h"
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Default Layer (QWERTY)
+    /* Default Layer
      * ,-----------------------------------------------------------.
      * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  Bsp  |
      * |-----------------------------------------------------------|
-     * |Fn1  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |
+     * | Tab |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |
      * |-----------------------------------------------------------|
-     * |Ctrl  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|  Fn2   |
+     * | Caps |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '| Enter  |
      * |-----------------------------------------------------------|
-     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |
+     * | Shift  |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|  Shift   |
      * |-----------------------------------------------------------|
-     * |Ctrl |GUI |Alt  |          Fn3            |Alt  |Fn0 |Ctrl |
+     * |Ctrl |Alt |GUI  |         Space           |Fn0  |Alt |Ctrl |
      * `-----------------------------------------------------------'
      */
     [0] = KEYMAP_AEK(
     ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,
-    FN1, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,
-    LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT, FN2,
+    TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,
+    CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,
     LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,
-    LCTL,LGUI,LALT,               FN3,                     RALT,FN0, RCTL
+    LCTL,LALT,LGUI,               SPC,                     FN0, RALT,RCTL
+    ),
+
+    /* QWERTY */
+    [1] = KEYMAP_AEK(
+    ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,
+    FN1, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,
+    CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT, FN2,
+    LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,
+    LCTL,LALT,LGUI,               FN3,                     FN0, RALT,RCTL
     ),
 
     /* Colemak */
-    [1] = KEYMAP_AEK(
+    [2] = KEYMAP_AEK(
     ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,
     FN1, Q,   W,   F,   P,   G,   J,   L,   U,   Y,   SCLN,LBRC,RBRC,BSLS,
-    LCTL,A,   R,   S,   T,   D,   H,   N,   E,   I,   O,   QUOT,FN2,
+    CAPS,A,   R,   S,   T,   D,   H,   N,   E,   I,   O,   QUOT,FN2,
     LSFT,Z,   X,   C,   V,   B,   K,   M,   COMM,DOT, SLSH,RSFT,
-    LCTL,LGUI,LALT,               FN3,                     RALT,FN0, RCTL
+    LCTL,LALT,LGUI,               FN3,                     FN0, RALT,RCTL
     ),
 
     /* Dvorak */
-    [2] = KEYMAP_AEK(
+    [3] = KEYMAP_AEK(
     ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   LBRC,RBRC,BSPC,
     FN1, QUOT,COMM,DOT, P,   Y,   F,   G,   C,   R,   L,   SLSH,EQL, BSLS,
-    LCTL,A,   O,   E,   U,   I,   D,   H,   T,   N,   S,   MINS,FN2,
+    CAPS,A,   O,   E,   U,   I,   D,   H,   T,   N,   S,   MINS,FN2,
     LSFT,SCLN,Q,   J,   K,   X,   B,   M,   W,   V,   Z,   RSFT,
-    LCTL,LGUI,LALT,               FN3,                     RALT,FN0, RCTL
+    LCTL,LALT,LGUI,               FN3,                     FN0, RALT,RCTL
     ),
 
     /* Workman */
-    [3] = KEYMAP_AEK(
+    [4] = KEYMAP_AEK(
     ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,
     FN1, Q,   D,   R,   W,   B,   J,   F,   U,   P,   SCLN,LBRC,RBRC,BSLS,
-    LCTL,A,   S,   H,   T,   G,   Y,   N,   E,   O,   I,   QUOT,FN2,
+    CAPS,A,   S,   H,   T,   G,   Y,   N,   E,   O,   I,   QUOT,FN2,
     LSFT,Z,   X,   M,   C,   V,   K,   L,   COMM,DOT, SLSH,RSFT,
-    LCTL,LGUI,LALT,               FN3,                     RALT,FN0, RCTL
+    LCTL,LALT,LGUI,               FN3,                     FN0, RALT,RCTL
     ),
 
     /**
@@ -54,12 +63,12 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *
      * A winkeyless QWERTY layout with tap layers disabled
      */
-    [4] = KEYMAP_AEK(
+    [5] = KEYMAP_AEK(
     ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,
-    FN1, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,
-    LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,
+    TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,
+    CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,
     LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,
-    LCTL,NO,  LALT,               SPC,                     RALT,FN0, RCTL
+    LCTL,LALT,NO,                 SPC,                     FN0, RALT,RCTL
     ),
 
     /* HHKB mode (HHKB Fn)
@@ -72,10 +81,10 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------|
      * |Shift   |   |   |   |   |   |  +|  -|End|PgD|Dow| Shift    |
      * |-----------------------------------------------------------|
-     * |Ctrl |GUI |Alt  |       Space             |Alt  |Fn5 |Ctrl |
+     * |Ctrl |Alt |GUI  |       Space             |Fn0  |Alt |Ctrl |
      * `-----------------------------------------------------------'
      */ 
-    [5] = KEYMAP_AEK(
+    [6] = KEYMAP_AEK(
     GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, DEL,
     CAPS,NO,  NO,  NO,  NO,  NO,  NO,  NO,  PSCR,SLCK,PAUS,UP,  NO,  INS,
     TRNS,VOLD,VOLU,MUTE,NO,  NO,  PAST,PSLS,HOME,PGUP,LEFT,RGHT,TRNS,
@@ -83,12 +92,13 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TRNS,TRNS,TRNS,               TRNS,                    TRNS,TRNS,TRNS
    ),
 
-    [6] = KEYMAP_AEK(
+    /* Layout selection layer */
+    [7] = KEYMAP_AEK(
     NO,  FN4, FN5, FN6, FN7, FN8, NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,
     TRNS,FN4, FN7, NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,
     NO,  NO,  NO,  FN6, NO,  FN8, NO,  NO,  NO,  NO,  NO,  NO,  NO,
     NO,  NO,  NO,  FN5, NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,
-    NO,  NO,  NO,                 NO,                      NO,  TRNS,NO
+    NO,  NO,  NO,                 NO,                      TRNS,NO,  NO
     )
 };
 
@@ -98,13 +108,13 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Fn action definition
  */
 const uint16_t PROGMEM fn_actions[] = {
-    [0] = ACTION_LAYER_TAP_KEY(6, KC_RGUI),           // Layout selection layer
-    [1] = ACTION_LAYER_TAP_KEY(6, KC_TAB),            // Layout selection layer
-    [2] = ACTION_LAYER_TAP_KEY(5, KC_ENTER),          // HHKB layer
-    [3] = ACTION_LAYER_TAP_KEY(5, KC_SPC),            // HHKB layer
-    [4] = ACTION_DEFAULT_LAYER_SET(0),                // QWERTY layout
-    [5] = ACTION_DEFAULT_LAYER_SET(1),                // Colemak layout
-    [6] = ACTION_DEFAULT_LAYER_SET(2),                // Dvorak layout
-    [7] = ACTION_DEFAULT_LAYER_SET(3),                // Workman layout
-    [8] = ACTION_DEFAULT_LAYER_SET(4),                // Gamer layout
+    [0] = ACTION_LAYER_TAP_KEY(7, KC_RGUI),           // Layout selection layer
+    [1] = ACTION_LAYER_TAP_KEY(7, KC_TAB),            // Layout selection layer
+    [2] = ACTION_LAYER_TAP_KEY(6, KC_ENTER),          // HHKB layer
+    [3] = ACTION_LAYER_TAP_KEY(6, KC_SPC),            // HHKB layer
+    [4] = ACTION_DEFAULT_LAYER_SET(1),                // QWERTY layout
+    [5] = ACTION_DEFAULT_LAYER_SET(2),                // Colemak layout
+    [6] = ACTION_DEFAULT_LAYER_SET(3),                // Dvorak layout
+    [7] = ACTION_DEFAULT_LAYER_SET(4),                // Workman layout
+    [8] = ACTION_DEFAULT_LAYER_SET(5),                // Gamer layout
 };
