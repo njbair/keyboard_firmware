f3fce285451a99bd6c7020a1c6ac1ff44528dee9
Nick Bair <njbair@gmail.com>
2016-03-11 10:03:32 -0500

    redoing layouts from scratch

--- keyboard/alps64/config.h
+++ keyboard/alps64/config.h
@@ -18,7 +18,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef CONFIG_H
 #define CONFIG_H
 
-
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x6464
@@ -43,25 +42,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define IS_COMMAND() ( \
     keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
 )
-
-
-
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-//#define NO_ACTION_MACRO
-//#define NO_ACTION_FUNCTION
 
 #endif
--- keyboard/alps64/keymaps/ansi.c
+++ keyboard/alps64/keymaps/ansi.c
@@ -11,7 +11,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------|
      * | Shift  |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|  Shift   |
      * |-----------------------------------------------------------|
-     * |Ctrl|GUI |Alt |         Space          |Alt |GUI |Fn0 |Ctrl|
+     * |Ctrl|GUI |Alt |         Space          |Alt |GUI |App |Ctrl|
      * `-----------------------------------------------------------'
      */
     [0] = KEYMAP_ANSI(
@@ -19,56 +19,25 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,
     CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,
     LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,
-    LCTL,LGUI,LALT,               SPC,                RALT,RGUI,FN0, RCTL
+    LCTL,LGUI,LALT,               SPC,                RALT,RGUI,APP, RCTL
     ),
 
-    /* QWERTY */
+    /* Dvorak */
     [1] = KEYMAP_ANSI(
-    ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,
-    FN1, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,
-    CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,FN2,
-    LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,
-    LCTL,LGUI,LALT,               FN3,                RALT,RGUI,FN0, RCTL
+    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,LBRC,RBRC,TRNS,
+    TRNS,QUOT,COMM,DOT, P,   Y,   F,   G,   C,   R,   L,   SLSH,EQL, TRNS,
+    TRNS,TRNS,O,   E,   U,   I,   D,   H,   T,   N,   S,   MINS,TRNS,
+    TRNS,SCLN,Q,   J,   K,   X,   B,   TRNS,W,   V,   Z,   TRNS,
+    TRNS,TRNS,TRNS,               TRNS,               TRNS,TRNS,TRNS,TRNS
     ),
 
     /* Colemak */
     [2] = KEYMAP_ANSI(
-    ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,
-    FN1, Q,   W,   F,   P,   G,   J,   L,   U,   Y,   SCLN,LBRC,RBRC,BSLS,
-    CAPS,A,   R,   S,   T,   D,   H,   N,   E,   I,   O,   QUOT,FN2,
-    LSFT,Z,   X,   C,   V,   B,   K,   M,   COMM,DOT, SLSH,RSFT,
-    LCTL,LGUI,LALT,               FN3,                RALT,RGUI,FN0, RCTL
-    ),
-
-    /* Dvorak */
-    [3] = KEYMAP_ANSI(
-    ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   LBRC,RBRC,BSPC,
-    FN1, QUOT,COMM,DOT, P,   Y,   F,   G,   C,   R,   L,   SLSH,EQL, BSLS,
-    CAPS,A,   O,   E,   U,   I,   D,   H,   T,   N,   S,   MINS,FN2,
-    LSFT,SCLN,Q,   J,   K,   X,   B,   M,   W,   V,   Z,   RSFT,
-    LCTL,LGUI,LALT,               FN3,                RALT,RGUI,FN0, RCTL
-    ),
-
-    /* Workman */
-    [4] = KEYMAP_ANSI(
-    ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,
-    FN1, Q,   D,   R,   W,   B,   J,   F,   U,   P,   SCLN,LBRC,RBRC,BSLS,
-    CAPS,A,   S,   H,   T,   G,   Y,   N,   E,   O,   I,   QUOT,FN2,
-    LSFT,Z,   X,   M,   C,   V,   K,   L,   COMM,DOT, SLSH,RSFT,
-    LCTL,LGUI,LALT,               FN3,                RALT,RGUI,FN0, RCTL
-    ),
-
-    /**
-     * Gamer Layout
-     *
-     * A winkeyless QWERTY layout with tap layers disabled
-     */
-    [5] = KEYMAP_ANSI(
-    ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,
-    TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,
-    CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,
-    LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,
-    LCTL,NO,  LALT,               SPC,                RALT,NO,  FN0, RCTL
+    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+    TRNS,TRNS,TRNS,F,   P,   G,   J,   L,   U,   Y,   SCLN,TRNS,TRNS,TRNS,
+    TRNS,TRNS,R,   S,   T,   D,   TRNS,N,   E,   I,   O,   TRNS,TRNS,
+    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,K,   TRNS,TRNS,TRNS,TRNS,TRNS,
+    TRNS,TRNS,TRNS,               TRNS,               TRNS,TRNS,TRNS,TRNS
     ),
 
     /* HHKB mode (HHKB Fn)
@@ -81,10 +50,10 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------|
      * |Shift   |   |   |   |   |   |  +|  -|End|PgD|Dow| Shift    |
      * |-----------------------------------------------------------|
-     * |Ctrl|GUI |Alt |       Space            |Alt |GUI |Fn0 |Ctrl|
+     * |Ctrl|GUI |Alt |       Space            |Alt |GUI |Fn  |Ctrl|
      * `-----------------------------------------------------------'
-     */ 
-    [6] = KEYMAP_ANSI(
+     */
+    [20] = KEYMAP_ANSI(
     GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, DEL,
     CAPS,NO,  NO,  NO,  NO,  NO,  NO,  NO,  PSCR,SLCK,PAUS,UP,  NO,  INS,
     TRNS,VOLD,VOLU,MUTE,NO,  NO,  PAST,PSLS,HOME,PGUP,LEFT,RGHT,TRNS,
@@ -93,13 +62,22 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     /* Layout selection layer */
-    [7] = KEYMAP_ANSI(
-    NO,  FN4, FN5, FN6, FN7, FN8, NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,
-    TRNS,FN4, FN7, NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,
-    NO,  NO,  NO,  FN6, NO,  FN8, NO,  NO,  NO,  NO,  NO,  NO,  NO,
-    NO,  NO,  NO,  FN5, NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,
-    NO,  NO,  NO,                 NO,                 NO,  NO,  TRNS,NO
-    )
+    [30] = KEYMAP_ANSI(
+    NO,  FN1, FN2, NO,  NO,  NO,  NO,  NO,  NO,  NO,  FN0, NO,  NO,  NO,
+    NO,  FN0, NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,
+    NO,  NO,  NO,  FN1, NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,
+    NO,  NO,  NO,  FN2, NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,
+    NO,  NO,  NO,                 NO,                 NO,  NO,  NO,  NO
+    ),
+
+    /* Standard Fn Keys */
+    [31] = KEYMAP_ANSI(
+    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+    FN11,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,FN12,
+    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+    TRNS,TRNS,TRNS,               FN13,               TRNS,TRNS,FN10,TRNS
+    ),
 };
 
 
@@ -108,13 +86,11 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Fn action definition
  */
 const uint16_t PROGMEM fn_actions[] = {
-    [0]  = ACTION_LAYER_TAP_KEY(7, KC_APP),            // Layout selection layer
-    [1]  = ACTION_LAYER_TAP_KEY(7, KC_TAB),            // Layout selection layer
-    [2]  = ACTION_LAYER_TAP_KEY(6, KC_ENTER),          // HHKB layer
-    [3]  = ACTION_LAYER_TAP_KEY(6, KC_SPC),            // HHKB layer
-    [4]  = ACTION_DEFAULT_LAYER_SET(1),                // QWERTY layout
-    [5]  = ACTION_DEFAULT_LAYER_SET(2),                // Colemak layout
-    [6]  = ACTION_DEFAULT_LAYER_SET(3),                // Dvorak layout
-    [7]  = ACTION_DEFAULT_LAYER_SET(4),                // Workman layout
-    [8]  = ACTION_DEFAULT_LAYER_SET(5),                // Gamer layout
+    [0]  = ACTION_DEFAULT_LAYER_SET(0),                // QWERTY layout
+    [1]  = ACTION_DEFAULT_LAYER_SET(1),                // Dvorak layout
+    [2]  = ACTION_DEFAULT_LAYER_SET(2),                // Colemak layout
+    [10] = ACTION_LAYER_TAP_KEY(30, KC_APP),           // Layout selection layer
+    [11] = ACTION_LAYER_TAP_KEY(30, KC_TAB),           // Layout selection layer
+    [12] = ACTION_LAYER_TAP_KEY(20, KC_ENTER),         // HHKB layer
+    [13] = ACTION_LAYER_TAP_KEY(20, KC_SPC),           // HHKB layer
 };
