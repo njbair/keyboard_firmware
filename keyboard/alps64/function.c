3aafe9c5630cfc6517b5614017e3303092b08ea2
Nick Bair <njbair@gmail.com>
2016-03-13 18:51:53 -0400

    clean up macros and functions

--- keyboard/alps64/function.c
+++ keyboard/alps64/function.c
@@ -10,33 +10,10 @@ enum function_opt {
 };
 
 void keymap_call_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
+    /*
     switch (id) {
-        case LAYOUT_QWERTY:
-            if (record->event.pressed) {
-                layer_clear();
-                default_layer_set_state(0);
-                layer_on(31);
-            }
-            break;
-        case LAYOUT_DVORAK:
-            if (record->event.pressed) {
-                layer_clear();
-                default_layer_set_state(1);
-                layer_on(31);
-            }
-            break;
-        case LAYOUT_COLEMAK:
-            if (record->event.pressed) {
-                layer_clear();
-                default_layer_set_state(2);
-                layer_on(31);
-            }
-            break;
-        case LAYOUT_GAMER:
-            if (record->event.pressed) {
-                layer_clear();
-                default_layer_set_state(3);
-            }
-            break;
+        case SOME_ID:
+            // do stuff
     }
+    */
 };
