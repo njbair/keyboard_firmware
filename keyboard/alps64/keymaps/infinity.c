9c1c588c0b712e482ef0ca7894219d62e117a82b
Nick Bair <njbair@gmail.com>
2016-03-17 18:35:06 -0400

    add infinity layout

---
+++ keyboard/alps64/keymaps/infinity.c
@@ -0,0 +1,114 @@
+#include "keymap_common.h"
+#include "macro.c"
+#include "function.c"
+
+const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /**
+     * Default Layer
+     * ,-----------------------------------------------------------.
+     * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  \|  `|
+     * |-----------------------------------------------------------|
+     * | Tab |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]| Bsp |
+     * |-----------------------------------------------------------|
+     * | Ctrl |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '| Enter  |
+     * |-----------------------------------------------------------|
+     * | Shift  |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |Fn2|
+     * `-----------------------------------------------------------'
+     *       |GUI|Alt  |         Space         |Alt  |Fn0|
+     *       `-------------------------------------------'
+     */
+    [0] = KEYMAP_INFINITY(
+    ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV,
+    TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,
+    CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,
+    LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,NO,
+    LCTL,LGUI,LALT,               SPC,           RALT,RGUI,APP, RCTL
+    ),
+
+    /* Dvorak */
+    [1] = KEYMAP_INFINITY(
+    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,LBRC,RBRC,TRNS,TRNS,
+    TRNS,QUOT,COMM,DOT, P,   Y,   F,   G,   C,   R,   L,   SLSH,EQL, TRNS,
+    TRNS,TRNS,O,   E,   U,   I,   D,   H,   T,   N,   S,   MINS,TRNS,
+    TRNS,SCLN,Q,   J,   K,   X,   B,   TRNS,W,   V,   Z,   TRNS,TRNS,
+    TRNS,TRNS,TRNS,               TRNS,          TRNS,TRNS,TRNS,TRNS
+    ),
+
+    /* Colemak */
+    [2] = KEYMAP_INFINITY(
+    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+    TRNS,TRNS,TRNS,F,   P,   G,   J,   L,   U,   Y,   SCLN,TRNS,TRNS,TRNS,
+    TRNS,TRNS,R,   S,   T,   D,   TRNS,N,   E,   I,   O,   TRNS,TRNS,
+    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,K,   TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+    TRNS,TRNS,TRNS,               TRNS,          TRNS,TRNS,TRNS,TRNS
+    ),
+
+    /**
+     * HHKB mode (HHKB Fn)
+     * ,-----------------------------------------------------------.
+     * |Esc| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|   |Ins|
+     * |-----------------------------------------------------------|
+     * |Caps |   |   |   |   |   |   |   |Psc|Slk|Pus|Up |   | Del |
+     * |-----------------------------------------------------------|
+     * |Ctrl  |VoD|VoU|Mut|   |   |  *|  /|Hom|PgU|Lef|Rig| Enter  |
+     * |-----------------------------------------------------------|
+     * |Shift   |   |   |   |   |   |  +|  -|End|PgD|Dow|Shift |   |
+     * `-----------------------------------------------------------'
+     *       |GUI|Alt  |        Space          |Alt  |Fn0|
+     *       `-------------------------------------------'
+     */
+    [8] = KEYMAP_INFINITY(
+    TRNS,F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, NO,  INS,
+    CAPS,NO,  NO,  NO,  NO,  NO,  NO,  NO,  PSCR,SLCK,PAUS,UP,  NO,  DEL,
+    TRNS,VOLD,VOLU,MUTE,NO,  NO,  PAST,PSLS,HOME,PGUP,LEFT,RGHT,TRNS,
+    TRNS,NO,  NO,  NO,  NO,  NO,  PPLS,PMNS,END, PGDN,DOWN,TRNS,TRNS,
+    TRNS,TRNS,TRNS,               TRNS,          TRNS,TRNS,TRNS,TRNS
+    ),
+
+    /* SpaceFn & EnterFn */
+    [24] = KEYMAP_INFINITY(
+    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,FN9,
+    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+    TRNS,TRNS,TRNS,               FN8,           TRNS,TRNS,TRNS,TRNS
+    ),
+
+    /* Keymap settings layer */
+    [30] = KEYMAP_INFINITY(
+    NO,  FN1, FN2, NO,  NO,  NO,  NO,  NO,  NO,  NO,  FN0, NO,  NO,  NO,  NO,
+    NO,  FN0, NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,
+    NO,  NO,  NO,  FN1, NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  FN17,
+    NO,  NO,  NO,  FN2, NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,
+    NO,  NO,  NO,                 FN17,          NO,  NO,  NO,  NO
+    ),
+
+    /* Primary Fn Layer */
+    [31] = KEYMAP_INFINITY(
+    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,FN10,
+    TRNS,TRNS,TRNS,               TRNS,          TRNS,TRNS,FN16,TRNS
+    ),
+};
+
+
+
+/*
+ * Fn action definition
+ */
+const uint16_t PROGMEM fn_actions[] = {
+    [0]  = ACTION_DEFAULT_LAYER_SET(0),             // QWERTY layout
+    [1]  = ACTION_DEFAULT_LAYER_SET(1),             // Dvorak layout
+    [2]  = ACTION_DEFAULT_LAYER_SET(2),             // Colemak layout
+
+    [8]  = ACTION_LAYER_TAP_KEY(8, KC_SPC),         // HHKB function layer
+    [9]  = ACTION_LAYER_TAP_KEY(8, KC_ENTER),       // HHKB function layer
+    [10] = ACTION_LAYER_MOMENTARY(8),               // HHKB function layer
+
+    [16] = ACTION_LAYER_TAP_KEY(30, KC_APP),        // Keymap settings layer
+    [17] = ACTION_LAYER_TOGGLE(24),                 // Toggle SpaceFn & EnterFn
+
+    [20] = ACTION_MACRO(SIGNATURE),                 // Forum auto-signature
+};
