8645478446ed03f71362869641d2d5f32a0a80f9
Nick Bair <njbair@gmail.com>
2016-04-02 14:00:19 -0400

    implement time interval-based hooks

--- keyboard/alps64/hooks.c
+++ keyboard/alps64/hooks.c
@@ -1,9 +1,20 @@
 #include "action_layer.h"
 #include "layer.h"
+#include "print.h"
 
 void hook_init_late(void)
 {
     layer_on(L_FN_SPACEFN);
     layer_on(L_OVERLAY_CTRL_REMAP);
     layer_on(L_FN_PRIMARY);
+}
+
+void hook_interval_100ms()
+{
+    xprintf("100ms\n");
+}
+
+void hook_interval_1000ms()
+{
+    xprintf("1000ms\n");
 }
