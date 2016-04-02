7c0491ec23c668ebe5fbda5e46975616ae2ab09f
Nick Bair <njbair@gmail.com>
2016-04-02 15:25:54 -0400

    correct hooks comments

--- keyboard/alps64/hooks.c
+++ keyboard/alps64/hooks.c
@@ -8,13 +8,3 @@ void hook_init_late(void)
     layer_on(L_OVERLAY_CTRL_REMAP);
     layer_on(L_FN_PRIMARY);
 }
-
-void hook_interval_100ms()
-{
-    xprintf("100ms\n");
-}
-
-void hook_interval_1000ms()
-{
-    xprintf("1000ms\n");
-}
